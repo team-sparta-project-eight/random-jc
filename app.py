27ead284272c8c6e4dbe5f9df5db44e2b157af56
seok6086 <114342129+seok6086@users.noreply.github.com>
2022-11-15 12:42:12 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify , session
 app = Flask(__name__)
 
 from pymongo import MongoClient
-import certifi
-
-ca = certifi.where()
-
-client = MongoClient('mongodb+srv://test:sparta@cluster0.pkdgkh7.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
+client = MongoClient('mongodb+srv://test:sparta@cluster0.vkxpfdw.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route("/https://starbugs.herokuapp.com/api/menus", methods=["POST"])
+def web_result_page_post():
+    id_receive = request.form['id_give']
+    name_receive = request.form['name_give']
+    image_receive = request.form['image_give']
+    doc = {
+        'id' : id_receive,
+        'name' : name_receive,
+        'image' : image_receive
+    }
+    db.product.insert_one(doc)
+    return jsonify({'msg': '주문 완료!'})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
+
 
